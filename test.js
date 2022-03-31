0c45565ec9b8d8f7b18214e28b870cefed36e181
Julien <45082899+JulienBouchardIT@users.noreply.github.com>
2022-03-31 17:17:20 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,26 +1,18 @@
 import http from 'k6/http';
 
-import { Rate } from 'k6/metrics';
-import { check } from 'k6';
-
-const failRate = new Rate('failed_requests');
-
 export const options = {
-  thresholds: {
-    failed_requests: ['rate<=0'],
-    http_req_duration: ['p(95)<500'],
-  },
-  ext: {
-    loadimpact: {
-      projectID: 3568563,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+      duration: '30s',
+      preAllocatedVUs: 100, // how large the initial pool of VUs would be
+      maxVUs: 200, // if the preAllocatedVUs are not enough, we can initialize more
     },
   },
 };
 
 export default function () {
-  const result = http.get('https://test-api.k6.io');
-  check(result, {
-    'http response status code is 200': result.status === 200,
-  });
-  failRate.add(result.status !== 200);
+  http.get('http://test.k6.io/contacts.php');
 }
